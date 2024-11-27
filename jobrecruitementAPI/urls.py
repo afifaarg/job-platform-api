119c4e0d013a6597052d6272679e08f9077af5f6
afifaarg <argoubafifa1@gmail.com>
2024-11-27 20:36:11 +0100

    fix login12679

--- jobrecruitementAPI/urls.py
+++ jobrecruitementAPI/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LoginView, PlatformUserViewSet, LogoutView, fetchUsers, register_admin  # Adjust the import as needed
+from .views import LoginView, PlatformUserViewSet, LogoutView, FetchUsers, register_admin  # Adjust the import as needed
 
 router = DefaultRouter()
 router.register('users', PlatformUserViewSet, basename='users') 
